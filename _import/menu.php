6b0534c5d04598ac23c11599dfe78f23aa31084e
Denis Sidilkovsky <sudilkovsky@gmail.com>
2015-10-15 12:07:25 +0300

    test

---
+++ _import/menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+
+    <div class="navbar-header">
+	    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      
+      <a class="navbar-brand" href="#">Supernova</a>
+    </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-12">
+     
+      <ul class="nav navbar-nav navbar-right">
+        <li class="disabled"><a href="#">Инфо</a></li>
+        <li class="disabled"><a href="#">Нави</a></li>
+        <li><a href="#">Сетка</a></li>
+        <li class="disabled"><a href="#">Структура</a></li>
+        <li><a href="#">Лейауты</a></li>
+        <li><a href="#">Типографика</a></li>
+        <li><a href="#">Цвет</a></li>
+        <li><a href="#">Компоненты</a></li>
+        <li class="disabled"><a href="#">Графика</a></li>
+        <li class="disabled"><a href="#">Анимация</a></li>
+        <li class="disabled"><a href="#">Реклама</a></li>
+        
+      </ul>
+    </div>
+  </div>
+</nav> 
