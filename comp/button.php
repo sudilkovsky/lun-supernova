96ce12e0fa1790e7bb1b29a271c0f7dd6ae1c4d3
Denis Sidilkovsky <sudilkovsky@gmail.com>
2015-10-23 15:08:09 +0300

    add Button article

---
+++ comp/button.php
@@ -0,0 +1,82 @@
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/head.html');
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/menu.html');
+    ?>
+
+<title>Кнопки</title>
+
+<style>
+</style>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+				
+			<div class="page-header">
+			  <h1>Кнопки  <small>Плоские, объемные</small></h1>
+			</div>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+
+			<p>Кнопки двух типов: Плоские и Объемные.</p>
+			
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/button/button-example.png" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<hr>
+			<h3>Структура и отступы</h3>
+		</div>
+	</div>
+	<div class="row mt_xl">
+		<div class="col-sm-4">
+			<p>Текст кнопок стандартного размера набран шрифтом параграфа в начертании <code>semi-bold</code>.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/button/button-structure.png" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row mt_s">
+		<div class="col-sm-4">
+			<p>Объединяя кнопки в группы ключевое действие размещаем справа.</p>
+			<p>Не рекомендуется объединять плоские и объемные кнопки в одну группу.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/button/button-group-structure.png" class="img-responsive" />
+		</div>
+	</div>
+	
+	
+	
+	<div class="row mt_xl">
+		<div class="col-sm-12">
+			<hr>
+			<h3>Размеры кнопок</h3>
+		</div>
+	</div>
+	<div class="row mt_xl">
+		<div class="col-sm-4">
+			<p>Размер М &mdash; стандартный. В крайних случаях при необходимости используем альтернативные варианты размеров кнопок.</p>
+			<p>В любом из произвольных размеров высота кнопки должна быть кратной 4 для сохранения вертикального ритма.</p>
+
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/button/button-size.png" class="img-responsive" />
+		</div>
+	</div>
+
+	
+
+</div>
+
+
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/foot.html');
+?>
