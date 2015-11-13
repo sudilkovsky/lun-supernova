3b75d4b44b16b01612ee76bf29b0d9500673a152
Denis Sidilkovsky <sudilkovsky@gmail.com>
2015-11-13 12:45:59 +0200

    new Article about graphics and rich-media includes

---
+++ graph.php
@@ -0,0 +1,40 @@
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/head.html');
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/menu.html');
+    ?>
+
+<title>Графические элементы</title>
+
+<style>
+</style>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+				
+			<div class="page-header">
+			  <h1>Графические элементы <small>Изображения, Иконки</small></h1>
+			</div>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-8">
+			<h3>Пропорции</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+			<p>Все изображения имеют строгие пропорции 1:1&nbsp;или&nbsp;4:3.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/graph-example.png" class="img-responsive" />
+		</div>
+	</div>
+</div>
+
+
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/foot.html');
+?>
