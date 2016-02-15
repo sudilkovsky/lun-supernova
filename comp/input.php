38802090273136e846820f75319e76c2e9ff897f
Arsenii Feshchenko <ars.feshchenko@gmail.com>
2016-02-15 15:48:13 +0200

    Input tab

---
+++ comp/input.php
@@ -0,0 +1,85 @@
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/head.html');
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/menu.html');
+    ?>
+
+<title>Формы</title>
+
+<style>
+</style>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+
+			<div class="page-header">
+			  <h1>Поля ввода  <small>Плоские, объемные</small></h1>
+			</div>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-4">
+
+			<p>Поля ввода двух типов: Плоские и Объемные.</p>
+
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/input/input-example.png" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<hr>
+			<h3>Структура и отступы</h3>
+		</div>
+	</div>
+	<div class="row mt_xl">
+		<div class="col-sm-4">
+			<p>Текст поля ввода набран шрифтом параграфа в начертании <code>regular</code>.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/input/input-measure.png" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row mt_s">
+		<div class="col-sm-4">
+			<p>Плоское поле подсвечиваем при вводе.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/input/input-highlight.png" class="img-responsive" />
+		</div>
+	</div>
+
+
+    <div class="row">
+		<div class="col-sm-12">
+			<hr>
+			<h3>Плейсхолдеры и ошибки</h3>
+		</div>
+	</div>
+	<div class="row mt_xl">
+		<div class="col-sm-4">
+			<p>Текст поля ввода набран шрифтом параграфа в начертании <code>regular</code>.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/input/input-default.png" class="img-responsive" />
+		</div>
+	</div>
+	<div class="row mt_s">
+		<div class="col-sm-4">
+			<p>Плоское поле подсвечиваем при вводе.</p>
+		</div>
+		<div class="col-sm-8">
+			<img src="_img/comp/input/input-error.png" class="img-responsive" />
+		</div>
+	</div>
+
+
+</div>
+
+
+<?php
+    echo file_get_contents('http://sd-03.lundev.net/lun-supernova/_import/foot.html');
+?>
